2d65c98a096d795c87817d9d41d0675ae00b89b4
Linnyxie <xiejuhua13@163.com>
2021-05-22 17:04:39 +0800

    --task=1001837 --user=谢菊花 【---TAPD试用样例---】任务-1 https://www.tapd.cn/30694246/s/1004768 测试任务关联源码提交-1.

--- CommitTest.py
+++ CommitTest.py
@@ -7,4 +7,4 @@ print("无格式的时间：", now)
 now_format = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print("格式化的时间：", now_format)
 
-print("增加一点打印，确认提交后TAPD端的源码关联效果——1。")
+print("测试任务关联源码提交-1。")
